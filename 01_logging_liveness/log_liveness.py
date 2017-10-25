64de90d3af39a6031c4fcae6e0a06b81ea889ffa
Christopher Hewett <chewett@hotmail.co.uk>
2017-10-25 23:11:15 +0100

    Reformatted it to match blog

--- 01_logging_liveness/log_liveness.py
+++ 01_logging_liveness/log_liveness.py
@@ -1,19 +1,23 @@
 import logging
 import time
 
-
+# First lets set up the formatting of the logger
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+
+# Get the base logger and set the default level
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-fileHandler = logging.FileHandler("mainlogger.log")
-fileHandler.setFormatter(logFormatter)
-logger.addHandler(fileHandler)
-
+# Create a console handler to output the logging to the screen
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
 consoleHandler.setLevel(logging.INFO)
 logger.addHandler(consoleHandler)
+
+# Create a handler to store the logs to a file
+fileHandler = logging.FileHandler("mainlogger.log")
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
 
 logger.info("Starting script...")
 
