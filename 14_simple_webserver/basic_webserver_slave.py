37dff71c32e905fbfc3779deeb6aeea7d813c413
Christopher Hewett <chewett@hotmail.co.uk>
2019-04-03 21:29:18 +0100

    Started a basic webserver from bottle

---
+++ 14_simple_webserver/basic_webserver_slave.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import json
+import ConfigParser
+from RpiCluster.MainLogger import add_file_logger, logger
+from RpiCluster.RpiWebserverSlaveThread import RpiWebserverSlaveThread
+
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rpicluster.cfg'))
+
+socket_port = config.getint("slave", "socket_port")
+master_ip = config.get("slave", "master_ip")
+
+add_file_logger("webserver_slave.log")
+
+webserver_slave_thread = RpiWebserverSlaveThread(master_ip, socket_port)
+
+webserver_slave_thread.start()
+
+
+from bottle import route, run, template
+
+@route('/')
+def index():
+    return template('<html><body>See status.json</body></html>')
+
+@route('/status.json')
+def status_json():
+    return template(json.dumps(RpiWebserverSlaveThread.current_webserver_data))
+
+
+
+
+run(host='localhost', port=8123)
