9958759f7e4b804c23385ac0b31fae2310a856d1
Christopher Hewett <chewett@hotmail.co.uk>
2019-04-03 21:15:17 +0100

    Added 14

---
+++ 14_simple_webserver/basic_slave.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import ConfigParser
+from RpiCluster.MainLogger import add_file_logger, logger
+from RpiCluster.RpiBasicSlaveThread import RpiBasicSlaveThread
+
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rpicluster.cfg'))
+
+socket_port = config.getint("slave", "socket_port")
+master_ip = config.get("slave", "master_ip")
+
+add_file_logger("slave.log")
+
+basic_slave_thread = RpiBasicSlaveThread(master_ip, socket_port)
+
+basic_slave_thread.start()
+
