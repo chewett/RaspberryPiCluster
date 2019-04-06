3b5eced71658aa78d1b36d58f6fec7c7fd305a30
Christopher Hewett <chewett@hotmail.co.uk>
2019-04-06 21:18:47 +0100

    Made the webserver a bit nicer

--- 14_simple_webserver/basic_webserver_slave.py
+++ 14_simple_webserver/basic_webserver_slave.py
@@ -3,8 +3,9 @@
 import os
 import json
 import ConfigParser
-from RpiCluster.MainLogger import add_file_logger, logger
+from RpiCluster.MainLogger import add_file_logger
 from RpiCluster.RpiWebserverSlaveThread import RpiWebserverSlaveThread
+from bottle import route, run, template
 
 config = ConfigParser.ConfigParser()
 config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rpicluster.cfg'))
@@ -17,22 +18,12 @@ webserver_port = config.get("webserver", "webserver_port")
 add_file_logger("webserver_slave.log")
 
 webserver_slave_thread = RpiWebserverSlaveThread(master_ip, socket_port)
-
 webserver_slave_thread.start()
 
-
-from bottle import route, run, template
 
 @route('/')
 def index():
     return template("templates/ClusterHomepage.html",
                     info=json.dumps(RpiWebserverSlaveThread.current_webserver_data, indent=4, sort_keys=True))
-
-@route('/status.json')
-def status_json():
-    return template(json.dumps(RpiWebserverSlaveThread.current_webserver_data))
-
-
-
 
 run(host=webserver_host, port=webserver_port)
