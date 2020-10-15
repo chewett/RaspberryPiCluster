32e672fa7b3d63688b43f830cb80604766efe06b
Christopher Hewett <chewett@hotmail.co.uk>
2020-10-15 23:51:24 +0100

    Added minor comments to the rpi cluster

--- RpiCluster/RpiClusterExceptions.py
+++ RpiCluster/RpiClusterExceptions.py
@@ -1,6 +1,7 @@
 
 
 class DisconnectionException(Exception):
+    """ Exception to use when there is an issue and the other end has disconnected from the socket """
 
     def __init__(self, message):
         self.message = message
